9c3cbe62fb0e165bb493d3e488bc6eac687df72c
muhammadnorkholit <mnorkholit7@gmail.com>
2023-03-04 20:22:25 +0700

    p

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Absen;
+use App\Models\Ruangan;
+use App\Models\Siswa;
+use App\Models\Tahun_Ajaran;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -20,7 +23,7 @@ class AbsenController extends Controller
     }
 
     public function AbsenRuangUi(){
-    $ruangan = DB::table('ruangan')->get();
+    $ruangan = Ruangan::all();
         return view('absen.siapkanRuangan',compact('ruangan'));
     }
     public function AbsenRuang(Request $request)
@@ -29,28 +32,41 @@ class AbsenController extends Controller
             'sesi'=>'required',
             'ruangan'=>'required',
         ]);
-       $siswa =  DB::table('siswa')
-        ->select('siswa.*')
-        ->where('id_ruangan',$request->ruangan)
+       $siswa =  Siswa::where('id_ruangan',$request->ruangan)
         ->where('sesi',$request->sesi)
         ->get();
 
-        $cek = DB::table('absen')
-        ->where('absen.id_siswa',$siswa[0]->id)
-        ->whereDate('absen.waktu',date('Y-m-d'))
-        ->count()
-        ;
+        if(count($siswa) == 0){
+        return redirect()->back()->with('error', 'Data  Siswa Belum Lengkap');
 
-        // if($cek == 0){
-        if(!$cek){
-        return redirect()->back()->with('error', 'Ruangan tidak ditemukan');
         }
 
-        elseif($cek == 0){
+        $cek = Absen::where('id_siswa',$siswa[0]->id)
+        ->whereDate('created_at',date('Y-m-d'))
+        ->count()
+        ;
+      
+
+        if($cek != 0){
+        return redirect()->back()->with('error', 'Ruangan tidak ditemukan');
+        }elseif($cek == 0){
+              $month = date('m');
+                if($month <= '06'){
+                    $tahun = date('Y',strtotime("-1 Year"))."/".date('Y');;
+                    $semester = "genap";
+                }else{
+                    $tahun = date('Y')."/".date('Y',strtotime("+1 year"));
+                    $semester = "ganjil";
+                }
+                $id_ajaran = Tahun_Ajaran::where('tahun',$tahun)->where('semester',$semester)->first()->id;
+
+                if(!$id_ajaran)return;
+        
             foreach ($siswa as $r ) {
-                DB::table('absen')->insert([
+                Absen::create([
                     'id_siswa'=>$r->id,
-                    'status'=>5
+                    'status'=>"belum hadir",
+                    'id_ajaran'=>$id_ajaran
                 ]);
         }
 
