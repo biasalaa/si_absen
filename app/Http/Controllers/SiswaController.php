d40fae7381bb798664134797a3ff4068cd276d79
muhammadnorkholit <mnorkholit7@gmail.com>
2023-03-04 08:45:39 +0700

    p

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jurusan;
+use App\Models\Ruangan;
 use App\Models\Siswa;
+use App\Models\Tahun_Ajaran;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
@@ -14,7 +17,12 @@ class SiswaController extends Controller
      */
     public function index()
     {
-        //
+         $cari = Request()->cari;
+        $data = Siswa::paginate(20);
+        if ($cari) {
+        $data = Siswa::where('nama_siswa','like','%'.$cari)->paginate(20);
+        }
+        return view('siswa.index', compact('data','cari'));
     }
 
     /**
@@ -24,7 +32,9 @@ class SiswaController extends Controller
      */
     public function create()
     {
-        //
+        $jurusan = Jurusan::all();
+        $ruangan = Ruangan::all();
+         return view('siswa.create',compact('jurusan','ruangan'));
     }
 
     /**
@@ -35,16 +45,56 @@ class SiswaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         Request()->validate(
+            [
+                'nama_siswa'=>'required',
+                'nisn'=>'required|numeric',
+                'id_jurusan'=>'required',
+                'id_ruangan'=>'required',
+                'sesi'=>'required',
+                'no_kelas'=>'required',
+                'tingkatan'=>'required',
+            ],
+            [
+                'nama_siswa.required'=>'Nama Wajib Diisi',
+                'nisn.required'=>'Nisn Wajib Diisi',
+                'id_jurusan.required'=>'Jurusan Wajib Diisi',
+                'id_ruangan.required'=>'Ruangan Wajib Diisi',
+                'sesi.required'=>'Sesi Wajib Diisi',
+                'no_kelas.required'=>'Nomor Kelas Wajib Diisi',
+                'tingkatan.required'=>'Tingkatan Kelas Wajib Diisi',
+            ]
+        );
+
+         $month = date('m');
+        if($month <= '06'){
+            $tahun = date('Y',strtotime("-1 Year"))."/".date('Y');;
+            $semester = "genap";
+        }else{
+             $tahun = date('Y')."/".date('Y',strtotime("+1 year"));
+            $semester = "ganjil";
+        }
+        $id_ajaran = Tahun_Ajaran::where('tahun',$tahun)->where('semester',$semester)->first()->id;
+
+        if(!$id_ajaran)return;
+
+        Request()['id_ajaran'] =  $id_ajaran;
+
+
+        // insert data to database
+        Siswa::create(Request()->all());
+
+
+        return redirect('/siswa')->with('success','Berhasil Menambah Siswa');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Siswa  $siswa
+     * @param  \App\Models\User  $User
      * @return \Illuminate\Http\Response
      */
-    public function show(Siswa $siswa)
+    public function show(User $User)
     {
         //
     }
@@ -52,34 +102,79 @@ class SiswaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Siswa  $siswa
+     * @param  \App\Models\User  $User
      * @return \Illuminate\Http\Response
      */
-    public function edit(Siswa $siswa)
+    public function edit( $id)
     {
-        //
+         $jurusan = Jurusan::all();
+        $ruangan = Ruangan::all();
+        $data = Siswa::find($id);
+        return view('siswa.edit', compact('data','jurusan','ruangan'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Siswa  $siswa
+     * @param  \App\Models\User  $User
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Siswa $siswa)
+    public function update(Request $request, $id)
     {
-        //
+        
+      Request()->validate(
+            [
+                'nama_siswa'=>'required',
+                'nisn'=>'required|numeric',
+                'id_jurusan'=>'required',
+                'id_ruangan'=>'required',
+                'sesi'=>'required',
+                'no_kelas'=>'required',
+                'tingkatan'=>'required',
+            ],
+            [
+                'nama_siswa.required'=>'Nama Wajib Diisi',
+                'nisn.required'=>'Nisn Wajib Diisi',
+                'id_jurusan.required'=>'Jurusan Wajib Diisi',
+                'id_ruangan.required'=>'Ruangan Wajib Diisi',
+                'sesi.required'=>'Sesi Wajib Diisi',
+                'no_kelas.required'=>'Nomor Kelas Wajib Diisi',
+                'tingkatan.required'=>'Tingkatan Kelas Wajib Diisi',
+            ]
+        );
+
+         $month = date('m');
+        if($month <= '06'){
+            $tahun = date('Y',strtotime("-1 Year"))."/".date('Y');;
+            $semester = "genap";
+        }else{
+             $tahun = date('Y')."/".date('Y',strtotime("+1 year"));
+            $semester = "ganjil";
+        }
+        $id_ajaran = Tahun_Ajaran::where('tahun',$tahun)->where('semester',$semester)->first()->id;
+
+        if(!$id_ajaran)return;
+
+        Request()['id_ajaran'] =  $id_ajaran;
+
+
+        // insert data to database
+        Siswa::find($id)->update(Request()->all());
+
+
+        return redirect('/siswa')->with('success','Berhasil Mengubah Siswa');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Siswa  $siswa
+     * @param  \App\Models\User  $User
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Siswa $siswa)
+    public function destroy(User $User)
     {
-        //
+        $User->delete();
+        return redirect()->back()->with('success', 'Siswa Berhasil Dihapus');
     }
 }
---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,84 @@
+@extends('component.master')
+
+@section('header')
+    <div class="section-header">
+        <h1>Siswa</h1>
+    </div>
+@endsection
+
+@section('content')
+    <div class="section-body">
+        <div class="card">
+            <div class="card-header" style="justify-content: space-between">
+                <a href="{{ Request()->url() }}/create" class="btn btn-success" style="color:white ;">Tambah Data</a>
+                <form action="/operator" method="get">
+                    <input type="text" value="{{ $cari }}" name="cari" placeholder="Cari..."
+                        class="form-control " autofocus id="">
+                </form>
+            </div>
+            @if (session('success'))
+                <div class="alert alert-success alert-dismissible show fade m-2">
+                    <div class="alert-body">
+                        <button class="close" data-dismiss="alert">
+                            <span>&times;</span>
+                        </button>
+                        {{ session('success') }}
+                    </div>
+                </div>
+            @endif
+            <div class="card-body">
+                <div class="card">
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-md">
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama Siswa</th>
+                                    <th scope="col">NiSN</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Sesi</th>
+                                    <th scope="col">Ruangan</th>
+                                </tr>
+                                <tr>
+                                    @forelse ($data as $j)
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td> {{ $j->nama_siswa }} </td>
+                                        <td> {{ $j->nisn }} </td>
+                                        <td> {{ $j->tingkatan }} {{ $j->jurusan->jurusan }} {{ $j->no_kelas }} </td>
+                                        <td> {{ $j->sesi }} </td>
+                                        <td> {{ $j->ruangan->nama_ruangan }} </td>
+                                        <td>
+                                            <div class=" d-flex ">
+                                                <div class="m-1">
+                                                    <a href="{{ Request()->url() }}/{{ $j->id }}/edit"
+                                                        class="btn btn-warning"><i class="fas fa-edit"></i></a>
+                                                </div>
+                                                <div class="m-1">
+                                                    <form class="d-inline"
+                                                        action="{{ Request()->url() }}/{{ $j->id }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button class="btn btn-danger" type="submit"><i
+                                                                class="fas fa-trash"></i></button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="3" align="center">Data Belum Ada</td>
+                                </tr>
+                                @endforelse
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-right">
+                {{ $data->links() }}
+            </div>
+        </div>
+    </div>
+@endsection
