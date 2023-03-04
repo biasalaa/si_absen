be55f21cacd418a4e51707b6ad5dde4ad631bfa7
muhammadnorkholit <mnorkholit7@gmail.com>
2023-03-04 21:30:17 +0700

    p

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Absen;
+use App\Models\Link;
 use App\Models\Siswa;
+use App\Models\Tahun_Ajaran;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,14 +13,16 @@ class AuthController extends Controller
 {
     public function loginUI()
     {
+      $this->generateTahunAjaran();
         return view('auth.login');
     }
     public function loginUIAdmin()
-    {
+    { 
+      $this->generateTahunAjaran();
         return view('auth.admin');
     }
 
-    public function loginSiswa(Type $var = null)
+    public function loginSiswa()
     {
        Request()->validate([
             'nisn' => ['required', 'max:10']
@@ -32,13 +36,14 @@ class AuthController extends Controller
             ->count();
 
             if($count > 0){
-                Auth::login($data);
+                // Auth::login($data);
                 // $siswa = DB::table('siswa')->get();
                 // dd($siswa);
-                $cek = Siswa::where('id_siswa', Auth::id())->update([
+                $cek = Siswa::find( $data->id)->update([
                     "status" => 1,
                 ]);
-                return redirect('/ujian');
+                $link = Link::first();
+                return redirect($link->url);
             }else{
             return redirect()->back()->with('alert','Ruangan Belum Terdaftar');
 
@@ -49,7 +54,7 @@ class AuthController extends Controller
             return redirect()->back()->with('alert','NISN Tidak Terdaftar');
         }
     }
-    public function loginAdmin(Type $var = null)
+    public function loginAdmin()
     {
          Request()->validate([
             'username' => ['required'],
@@ -64,5 +69,31 @@ class AuthController extends Controller
         } else {
             return redirect()->back();
         }
+    }
+
+    public function generateTahunAjaran()
+    {
+          $month = date('m');
+        if($month <= '06'){
+            $tahun = date('Y',strtotime("-1 Year"))."/".date('Y');;
+            $semester = "genap";
+           
+        }else{
+             $tahun = date('Y')."/".date('Y',strtotime("+1 year"));
+            $semester = "ganjil";
+        }
+
+        $ada = Tahun_Ajaran::where('tahun',$tahun)->where('semester',$semester)->first();
+        if(!$ada){
+            Tahun_Ajaran::create([
+                'tahun'=>$tahun,
+                'semester'=>"ganjil",
+            ]);
+            Tahun_Ajaran::create([
+                'tahun'=>$tahun,
+                'semester'=>'genap',
+            ]);
+        }
+
     }
 }
